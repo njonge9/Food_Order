7df1e3a4652eced6fe3c8fb875351e8ab6e06f49
Njonge9 <frednjonge9@gmail.com>
2024-01-06 17:29:50 +0300

    Fix the typo error in users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true
   validates :email,
-    format: { with: URI::MailTo::EMAIL_REGRXP },
+    format: { with: URI::MailTo::EMAIL_REGEXP },
     uniqueness: { case_sensitive: false }
 end
