e06eff8b1f71535ee68653593255f11b8da0f2cd
Njonge9 <frednjonge9@gmail.com>
2024-01-06 17:39:52 +0300

    Remove the extra spaces before validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,13 @@ class User < ApplicationRecord
   # Relationship
   has_many :memberships, dependent: :destroy
   has_many :organizations, through: :memberships
+
+  before_validation :strip_extra_spaces
+
+  private
+
+  def strip_extra_spaces
+    self.name = self.name&.strip
+    self.email = self.email&.strip
+  end
 end
