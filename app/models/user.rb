417a00de12b00d405167028f7fd0667a093d2bab
Njonge9 <frednjonge9@gmail.com>
2024-01-06 17:27:06 +0300

    Write the user model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email,
+    format: { with: URI::MailTo::EMAIL_REGRXP },
+    uniqueness: { case_sensitive: false }
 end
