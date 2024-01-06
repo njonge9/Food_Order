e5c6742ccef4f948f97c4954103b691e61fdc048
Njonge9 <frednjonge9@gmail.com>
2024-01-06 17:34:11 +0300

    Build up the relationship between the models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   validates :email,
     format: { with: URI::MailTo::EMAIL_REGEXP },
     uniqueness: { case_sensitive: false }
+
+  # Relationship
+  has_many :memberships, dependent: :destroy
+  has_many :organizations, through: :memberships
 end
