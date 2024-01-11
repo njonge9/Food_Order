1e049aeeb3f25e4df62c3252ce4778b47d3b392d
Njonge9 <frednjonge9@gmail.com>
2024-01-11 10:45:29 +0300

    Authenticating the app_session token

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,12 @@ class User < ApplicationRecord
     user.app_sessions.create if user.authenticate(password)
   end
 
+  def authenticate_app_session(app_session_id, token)
+    app_sessions.find(app_session_id).authenticate_token(token)
+  rescue ActiveRecord::RecordNotFound
+    nil
+  end
+
   private
 
   def strip_extra_spaces
