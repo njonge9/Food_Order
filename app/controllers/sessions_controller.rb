a11075db3841281ef0da87867520cc4ce9fcd70b
Njonge9 <frednjonge9@gmail.com>
2024-01-11 11:01:19 +0300

    Skip authentication on the login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  skip_authentication only: [:new, :create]
+
   def new
   end
 
@@ -10,7 +12,7 @@ class SessionsController < ApplicationController
 
     if @app_session
       log_in(@app_session)
-      
+
       flash[:success] = t('.success')
       redirect_to root_path, status: :see_other
     else
